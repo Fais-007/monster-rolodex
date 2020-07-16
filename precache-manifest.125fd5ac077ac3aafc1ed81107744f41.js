5717b00cfede952c1855828e77701aa7c7e5a22a
Fais-007 <faisaliqbal669@gmail.com>
2020-07-17 04:46:07 +0530

    Updates

---
+++ precache-manifest.125fd5ac077ac3aafc1ed81107744f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d61ae82d7f5f3f794aeae91f14a4d0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "69959ede135ebcc80251",
+    "url": "/monster-rolodex/static/css/main.cca06689.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69959ede135ebcc80251",
+    "url": "/monster-rolodex/static/js/main.6b0f2eaa.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
